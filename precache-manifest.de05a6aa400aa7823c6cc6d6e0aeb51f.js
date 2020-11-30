a19f13e94d063da561c653d85b811ebf789bf3a2
Livan Ojito <livanojito@gmail.com>
2020-11-29 23:39:35 -0800

    Deployment

---
+++ precache-manifest.de05a6aa400aa7823c6cc6d6e0aeb51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cde7d4764785a66b61d32de4f4bd1d2",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a811cf53428824a2cc15",
+    "url": "/tic-tac-toe/static/css/main.d36abe49.chunk.css"
+  },
+  {
+    "revision": "b86d495a140dda1b09f7",
+    "url": "/tic-tac-toe/static/js/2.e53e7e80.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/tic-tac-toe/static/js/2.e53e7e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a811cf53428824a2cc15",
+    "url": "/tic-tac-toe/static/js/main.c3b196de.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
